86492a1df9aa12512b9432e95b111325c7de2fc5
Hansaka Sudusinghe <91568447+IT21279584@users.noreply.github.com>
2024-05-04 23:20:23 +0530

    Update RegistrationPage.jsx

--- nasa-api-app/src/components/RegistrationPage.jsx
+++ nasa-api-app/src/components/RegistrationPage.jsx
@@ -36,7 +36,7 @@ const RegistrationPage = () => {
 
     try {
       // Make an HTTP POST request to your backend API endpoint
-      await axios.post("http://localhost:8080/api/users/register", {
+      await axios.post("nasa-app-server-production.up.railway.app/api/users/register", {
         username,
         email,
         password,
